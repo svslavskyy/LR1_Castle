4ad94dafff0f6df7ea82f0ce022a6e57f2c42e64
svslavskyy <47915782+svslavskyy@users.noreply.github.com>
2020-11-30 16:27:10 +0200

    refactored project

---
+++ LR1_castle_test/test/com/Castle/CastleTest.java
@@ -0,0 +1,41 @@
+package com.Castle;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+
+public class CastleTest {
+
+    @Test
+    public void CastleInfoShouldBeCorrect() {
+        Castle castle = new Castle("Null");
+        Tower tower = new Tower("Main Tower",
+                new Security(new Squad(60, "Alpha"), 150));
+        try {
+            castle.addTower(tower);
+        } catch (AddNullTowerException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(castle.getCastleName(), "Null");
+        assertEquals(castle.getTowers().get(0).getTowerName(), "Main Tower");
+        assertEquals(castle.getTowers().get(0).getSecurity().getSquad().getsquadName(), "Alpha");
+        assertEquals(castle.getTowers().get(0).getSecurity().getSquad().getSquadCount(), 60, 0.01);
+
+    }
+
+
+
+//    @Test
+//    public void FailToCastleWithNull(){
+//
+//        Castle castle = new Castle("Null");
+//        Tower tower = null;
+//        Throwable exception = Assertions.assertThrows(AddNullTowerException.class, ()->{
+//            castle.addTower(tower);
+//        });
+//    }
+
+}
